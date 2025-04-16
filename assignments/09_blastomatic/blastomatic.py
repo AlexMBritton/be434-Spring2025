d897247820cebcf5085e6280147b608a832f6d97
AlexMBritton <71798061+AlexMBritton@users.noreply.github.com>
2025-04-16 21:30:15 +0000

    vfsa

--- assignments/09_blastomatic/blastomatic.py
+++ assignments/09_blastomatic/blastomatic.py
@@ -1,24 +1,17 @@
-
 #!/usr/bin/env python3
-"""Parse blast file"""
-
 import argparse
 import csv
-import os
 import sys
-
+import os
 
 def read_hits(file):
-    """Read BLAST hits from file"""
     if not os.path.isfile(file):
         print(f"No such file or directory: '{file}'")
         sys.exit(1)
     with open(file) as f:
         return [line.strip().split('\t') for line in f if line.strip()]
 
-
 def read_metadata(file):
-    """Read metadata from CSV file"""
     if not os.path.isfile(file):
         print(f"No such file or directory: '{file}'")
         sys.exit(1)
@@ -32,57 +25,22 @@ def read_metadata(file):
             meta[row['query']] = row
     return meta
 
-
-def guess_delimiter(filename, user_delim=None):
-    """Guess the delimiter based on file extension"""
+def guess_delimiter(filename, user_delim):
     if user_delim:
         return user_delim
     if filename.endswith(('.tsv', '.tab', '.txt')):
         return '\t'
     return ','
 
-
-def get_args():
-    """Get command-line arguments"""
-    parser = argparse.ArgumentParser(
-        description='Annotate BLAST output',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('-b',
-                       '--blasthits',
-                       metavar='FILE',
-                       help='BLAST -outfmt 6',
-                       required=True)
-
-    parser.add_argument('-a',
-                       '--annotations',
-                       metavar='FILE',
-                       help='Annotations file',
-                       required=True)
-
-    parser.add_argument('-o',
-                       '--outfile',
-                       metavar='FILE',
-                       help='Output file',
-                       default='out.csv')
-
-    parser.add_argument('-d',
-                       '--delimiter',
-                       metavar='DELIM',
-                       help='Output field delimiter')
-
-    parser.add_argument('-p',
-                       '--pctid',
-                       help='Minimum percent identity',
-                       type=float,
-                       default=0.0)
-
-    return parser.parse_args()
-
-
 def main():
-    """Make a jazz noise here"""
-    args = get_args()
+    parser = argparse.ArgumentParser(description='Merge BLAST hits with metadata')
+    parser.add_argument('-a', '--annotations', required=True, help='Metadata CSV file')
+    parser.add_argument('-b', '--blasthits', required=True, help='BLAST tabular hits file')
+    parser.add_argument('-o', '--outfile', type=str, default="out.csv", help='Output file (default: out.csv)')
+    parser.add_argument('-p', '--pctid', type=float, default=0, help='Minimum percent identity (default: 0)')
+    parser.add_argument('-d', '--delimiter', type=str, help='Output delimiter (default: guessed from filename)')
+    args = parser.parse_args()
+
     hits = read_hits(args.blasthits)
     metadata = read_metadata(args.annotations)
 
@@ -104,14 +62,11 @@ def main():
 
     delimiter = guess_delimiter(args.outfile, args.delimiter)
     with open(args.outfile, 'w', newline='') as out_f:
-        writer = csv.DictWriter(out_f,
-                              fieldnames=['qseqid', 'pident', 'depth', 'lat_lon'],
-                              delimiter=delimiter)
+        writer = csv.DictWriter(out_f, fieldnames=['qseqid', 'pident', 'depth', 'lat_lon'], delimiter=delimiter)
         writer.writeheader()
         writer.writerows(results)
 
-    print(f'Exported {len(results)} to "{args.outfile}".')
-
+    print(f'Exported {len(results)} to "{args.outfile}".', end='')
 
 if __name__ == '__main__':
     main()
